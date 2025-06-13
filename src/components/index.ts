1dfd82ae9fb4d8657c47683db27e250d906864a8
usmangq12 <usmangq12@gmail.com>
2025-06-14 01:46:28 +0500

    chore: Updated export of ui components as per Vite docs for lib

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,23 @@
-export * from "./Box/Box";
-export * from "./Container/Container";
-export * from "./Flex/Flex";
-export * from "./Grid/Grid";
-export * from "./Heading/Heading";
-export * from "./ExternalLink/ExternalLink";
-export * from "./Paragraph/Paragraph";
-export * from "./Section/Section";
-export * from "./Stack/Stack";
-export * from "./Text/Text";
+import { Box } from "./Box/Box";
+import { Container } from "./Container/Container";
+import { Flex } from "./Flex/Flex";
+import { Grid } from "./Grid/Grid";
+import { Heading } from "./Heading/Heading";
+import { ExternalLink } from "./ExternalLink/ExternalLink";
+import { Paragraph } from "./Paragraph/Paragraph";
+import { Section } from "./Section/Section";
+import { Stack } from "./Stack/Stack";
+import { Text } from "./Text/Text";
+
+export {
+  Box,
+  Container,
+  Flex,
+  Grid,
+  Heading,
+  ExternalLink,
+  Paragraph,
+  Section,
+  Stack,
+  Text,
+};
