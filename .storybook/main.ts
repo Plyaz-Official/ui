a531203d594f93da03511250a915285adad20149
usmangq12 <usmangq12@gmail.com>
2025-06-19 20:51:11 +0500

    chore:updated storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mdx|ts|tsx)', '../tailwind.config.js'],
   addons: [
@@ -15,13 +16,16 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
+
   docs: {
     autodocs: true,
   },
-   viteFinal: async () => {
-    return  {
+  viteFinal: async (config) => {
+    return {
+      ...config,
+      plugins: [...(config.plugins || [])],
       build: {
-        sourcemap: true, 
+        sourcemap: "hidden",
       },
     };
   },
