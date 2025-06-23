0c7632d551ea0b4a745f38a096866622e5ef52f8
usmangq12 <usmangq12@gmail.com>
2025-06-24 02:34:28 +0500

    chore: Use `jiti` to load `tailwind.config.js` for addon compatibility

---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+// Adds a JS shim to load the TS config at runtime, enabling
+// support for addons expecting tailwind.config.js.
+const jiti = require("jiti")(__dirname);
+module.exports = jiti("@plyaz/devtools/configs/tailwind.config").default;
--- tailwind.config.ts
+++
@@ -1,3 +0,0 @@
-import baseConfig from "@plyaz/devtools/configs/tailwind.config";
-
-export default baseConfig;
