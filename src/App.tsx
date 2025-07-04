85da5a015b6c29218c5e0afd97415fa1e21ba3b2
usmangq12 <usmangq12@gmail.com>
2025-07-04 18:01:21 +0500

    feat: Integrate MagicUi and ShadcnUi libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 "use client";
-import "./App.css";
 import { Box, ExternalLink, Flex, Grid } from "@/components";
+import { Button } from "@/components/Button/button";
+import "./App.css";
+import "./global.css";
 
 export type AppProps = {
   children?: React.ReactNode;
@@ -9,15 +11,16 @@ export type AppProps = {
 function App(): React.ReactElement {
   return (
     <Flex
-      className="flex min-h-screen items-center justify-center"
+      className="flex justify-center items-center min-h-screen"
       direction="col"
       gap="gap-4"
     >
       <Grid cols="grid-cols-2" element="main" gap="gap-2">
-        <Box className="rounded bg-blue-500 p-4">Item 1</Box>
-        <Box className="rounded bg-green-500 p-4">Item 2</Box>
-        <Box className="rounded bg-red-500 p-4">Item 3</Box>
-        <Box className="rounded bg-yellow-500 p-4">Item 4</Box>
+        <Box className="bg-blue-500 p-4 rounded">Item 1</Box>
+        <Box className="bg-green-500 p-4 rounded">Item 2</Box>
+        <Box className="bg-red-500 p-4 rounded">Item 3</Box>
+        <Box className="bg-yellow-500 p-4 rounded">Item 4</Box>
+        <Button className="bg-accent-foreground">title</Button>
       </Grid>
       <ExternalLink href="https:www.google.com"> This is the link</ExternalLink>
     </Flex>
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,53 +1,10 @@
-import { cva } from "class-variance-authority";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
 
-export const JUASTIFY_MAPPER = {
-  start: "justify-start",
-  end: "justify-end",
-  center: "justify-center",
-  between: "justify-between",
-  around: "justify-around",
-  evenly: "justify-evenly",
-  stretch: "justify-stretch",
-};
+import { cn } from "@/lib/utils";
 
-export const ALIGN_MAPPER = {
-  start: "items-start",
-  end: "items-end",
-  center: "items-center",
-  baseline: "items-baseline",
-  stretch: "items-stretch",
-};
-export const VARIANT_MAPPER = {
-  body: "font-sans",
-  heading: "font-sans",
-  caption: "font-sans",
-};
-
-export const SIZES_MAPPER = {
-  xs: "text-xs",
-  sm: "text-sm",
-  base: "text-base",
-  lg: "text-lg",
-  xl: "text-xl",
-  "2xl": "text-2xl",
-  "3xl": "text-3xl",
-  "4xl": "text-4xl",
-  "5xl": "text-5xl",
-  "6xl": "text-6xl",
-  "7xl": "text-7xl",
-  "8xl": "text-8xl",
-  "9xl": "text-9xl",
-};
-
-export const TEXT_WEIGHT_MAPPER = {
-  normal: "font-normal",
-  medium: "font-medium",
-  semibold: "font-semibold",
-  bold: "font-bold",
-  light: "font-light",
-} as const;
-
-export const buttonVariants = cva(
+const buttonVariants = cva(
   `inline-flex justify-center items-center gap-2 disabled:opacity-50 aria-invalid:border-destructive focus-visible:border-ring rounded-md outline-none aria-invalid:ring-destructive/20 focus-visible:ring-[3px] focus-visible:ring-ring/50 dark:aria-invalid:ring-destructive/40 [&_svg:not([class*='size-'])]:size-4 font-medium text-sm whitespace-nowrap transition-all [&_svg]:pointer-events-none disabled:pointer-events-none shrink-0 [&_svg]:shrink-0`,
   {
     variants: {
@@ -102,3 +59,26 @@ export const buttonVariants = cva(
     },
   }
 );
+
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : "button";
+
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export { Button, buttonVariants };
