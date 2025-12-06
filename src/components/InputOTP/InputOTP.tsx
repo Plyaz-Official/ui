2172a2ad9395593d6adf429bc5b820f9b4f96dbb
Marchionni Martin <tinchomarchi34@gmail.com>
2025-12-06 12:19:48 -0300

    refactor: Update InputOTP component styles for improved accessibility and visual consistency
    - Adjusted InputOTPSlot styles to enhance the appearance and accessibility by changing the border color and size.
    - Updated InputOTPSeparator to use a bullet character for better visual separation and added a secondary text color.

--- src/components/InputOTP/InputOTP.tsx
+++ src/components/InputOTP/InputOTP.tsx
@@ -44,7 +44,7 @@ function InputOTPSlot({
       data-slot='input-otp-slot'
       data-active={isActive}
       className={cn(
-        'data-[active=true]:border-ring data-[active=true]:ring-ring/50 data-[active=true]:aria-invalid:ring-destructive/20 dark:data-[active=true]:aria-invalid:ring-destructive/40 aria-invalid:border-destructive data-[active=true]:aria-invalid:border-destructive dark:bg-input/30 border-input relative flex h-9 w-9 items-center justify-center border-y border-r text-sm shadow-xs transition-all outline-none first:rounded-l-md first:border-l last:rounded-r-md data-[active=true]:z-10 data-[active=true]:ring-[3px]',
+        'text-secondary data-[active=true]:border-ring data-[active=true]:ring-ring/50 data-[active=true]:aria-invalid:ring-destructive/20 dark:data-[active=true]:aria-invalid:ring-destructive/40 aria-invalid:border-destructive data-[active=true]:aria-invalid:border-destructive dark:bg-input/30 border-subtle relative flex h-10 w-10 items-center justify-center border-y border-r text-sm shadow-xs transition-all outline-none first:rounded-l-md first:border-l last:rounded-r-md data-[active=true]:z-10 data-[active=true]:ring-[3px]',
         className
       )}
       {...props}
@@ -61,8 +61,13 @@ function InputOTPSlot({
 
 function InputOTPSeparator({ ...props }: React.ComponentProps<'div'>) {
   return (
-    <div data-slot='input-otp-separator' role='separator' {...props}>
-      <MinusIcon />
+    <div
+      data-slot='input-otp-separator'
+      role='separator'
+      className='text-secondary size-5 flex items-center justify-center'
+      {...props}
+    >
+      •
     </div>
   );
 }
